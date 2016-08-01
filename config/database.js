47702f44e92e9397f868a8c3b0d7d4f27c7926ba
Brian Keenan <brian.keenan@sap.com>
2016-08-01 16:10:38 +0200

    Update database.js

--- config/database.js
+++ config/database.js
@@ -2,5 +2,5 @@ module.exports = {
     //LOCAL
     //'url' : 'mongodb://localhost/windi' 
     //PRODUCTION
-    'url' : 'mongodb://windadmin:$HatGB14@ds011389.mlab.com:11389/heroku_bdtwpswg'
+    'url' : ''
 };
